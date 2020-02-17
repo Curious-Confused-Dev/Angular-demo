a7eb3565c0ce572dacf0e7b7be587e7674235fdf
anshdeep_chawla <a.chawla@easternenterprise.com>
2020-02-17 22:28:04 +0530

    [TASK] Styling selectors and different selectors under components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles : [`
+  .header {
+    color: green;
+  }
+  `]
 })
 export class AppComponent {
   
